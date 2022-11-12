39d8884215f07e6d742a9810d690d0ab5e6e951f
elron <elronkarni123@gmail.com>
2022-11-12 11:17:06 +0200

    ready to deploy 0.5

--- client/src/pages/api/programs.js
+++ client/src/pages/api/programs.js
@@ -5,7 +5,7 @@ export default async function handler(req, res) {
   switch (req.method) {
     case "POST":
       try {
-        console.log("CONNECTING TO MONGO");
+        res.json("CONNECTING TO MONGO");
         await connectMongo();
         console.log("GOT CONNECTED TO MONGO");
 
@@ -19,7 +19,9 @@ export default async function handler(req, res) {
 
     case "GET":
       try {
+        res.json("CONNECTING TO MONGO");
         await connectMongo();
+        res.json("GOT CONNECTED TO MONGO");
         const Programs = await Program.find();
         res.json(Programs);
       } catch (err) {
