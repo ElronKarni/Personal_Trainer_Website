d54118911f4dde189d8d708eca9920b0505410bd
elron <elronkarni123@gmail.com>
2023-01-20 10:58:24 +0200

    asdsa

--- client/src/lib/OptionsAndProgramsProps.tsx
+++ client/src/lib/OptionsAndProgramsProps.tsx
@@ -4,6 +4,7 @@ export default async function OptionsAndProgramsProps() {
   const options = await axios
     .get("https://lior-malul-trainer.vercel.app/api/options")
     .then((res) => res.data);
+
   const programs = await axios
     .get("https://lior-malul-trainer.vercel.app/api/programs")
     .then((res) => res.data);
